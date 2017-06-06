4998ff21d9fed755ceee1e0a113bda1f3d1ef549
bibby1101 <bibby1101@gmail.com>
2017-06-06 23:51:10 +0800

    check intent service lifecycle & communction with activity

--- app/src/main/java/com/bibby/testintentservice/MainActivity.java
+++ app/src/main/java/com/bibby/testintentservice/MainActivity.java
@@ -1,13 +1,68 @@
 package com.bibby.testintentservice;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements MyResultReceiver.Receiver{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    TextView state;
+    public MyResultReceiver myResultReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        state = (TextView) this.findViewById(R.id.state);
+
+        state.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "click to bind service, thread id : " + Thread.currentThread().getId());
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, TvHIDService.class);
+                intent.putExtra("receiver", myResultReceiver);
+                startService(intent);
+            }
+        });
+
+        myResultReceiver = new MyResultReceiver(new Handler());
+        myResultReceiver.setReceiver(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onReceiveResult(int resultCode, Bundle resultData) {
+        Log.d(TAG, "onReceiveResult, thread id : " + Thread.currentThread().getId());
+        Log.d(TAG, "onReceiveResult : " + resultCode + " | " + resultData.getString("Service"));
     }
 }
--- app/src/main/java/com/bibby/testintentservice/TvHIDService.java
+++ app/src/main/java/com/bibby/testintentservice/TvHIDService.java
@@ -2,13 +2,18 @@ package com.bibby.testintentservice;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.os.ResultReceiver;
 import android.support.annotation.Nullable;
 import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
 
 public class TvHIDService extends IntentService {
 
     private final static String TAG = TvHIDService.class.getSimpleName();
+    private int i = 0;
 
     /**
      * Creates an IntentService.
@@ -26,42 +31,63 @@ public class TvHIDService extends IntentService {
      */
     public TvHIDService(String name) {
         super(name);
-        Log.d(TAG, "onConstructed");
+        Log.d(TAG, "onConstructed, thread id : " + Thread.currentThread().getId());
     }
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        Log.d(TAG, "onHandleIntent");
+        Log.d(TAG, "onHandleIntent, thread id : " + Thread.currentThread().getId());
+
+        ResultReceiver rec = intent.getParcelableExtra("receiver");
+
+        Log.d(TAG, "onHandleIntent(), start work.");
+        Log.d(TAG, "print start, init i = " + i);
+        for(; i< 5; i++) {
+            Log.d(TAG, "i = " + i);
+
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.d(TAG, "onHandleIntent(), end work.");
+
+        Bundle b = new Bundle();
+        b.putString("Service","i = "+i);
+        rec.send(9527, b);
     }
 
     @Override
     public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
-        Log.d(TAG, "onStartCommand");
+        Log.d(TAG, "onStartCommand, thread id : " + Thread.currentThread().getId());
+
+//        return START_STICKY;
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        Log.d(TAG, "onBind");
+        Log.d(TAG, "onBind, thread id : " + Thread.currentThread().getId());
         return super.onBind(intent);
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        Log.d(TAG, "onUnbind");
+        Log.d(TAG, "onUnbind, thread id : " + Thread.currentThread().getId());
         return super.onUnbind(intent);
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d(TAG, "onCreate");
+        Log.d(TAG, "onCreate, thread id : " + Thread.currentThread().getId());
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy");
+        Log.d(TAG, "onDestroy, thread id : " + Thread.currentThread().getId());
     }
 }
